c1e12f918cddcb1900ff5ca7d642ec6f2a10dbfe
parasu <parasumara@gmail.com>
2022-12-28 13:52:58 +0900

    サイト　メンバー紹介tmp

---
+++ desktop4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>テスト</title>
+</head>
+<body>
+<h1>3pt チーム　卒業制作ホームページ</h1>
+
+<h2>メンバー紹介</h2><br>
+
+<h2>我妻　大輝</h2>
+
+<ol></ol>コメント
+
+<h2>菊池　佳太</h2>
+
+<ol></ol>コメント
+
+<h2>香西　史郁</h2>
+
+<ol></ol>コメント
+
+<h2>田中　慶紀</h2>
+
+<ol></ol>コメント
+</table>
+</body>
+</html>
