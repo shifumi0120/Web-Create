ddb348135ca4dc76de702fed5b5c6b085fe2b90a
daaa0217 <gochiusa2002@gmail.com>
2023-01-12 09:31:57 +0900

    メニュー　基本は出来ている　あとは画面の調整

---
+++ site.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>サイト・メニュー画面</title>
+</head>
+<body>
+<font size="7">
+3pt チーム　卒業制作ホームページ
+</font>
+<hr></hr>
+<font size="7">
+<p style="text-align: left">
+<a href="page2.php">・TOP</a>
+</p>
+</font>
+
+<font size="7">
+<p style="text-align: left">
+<a href="page2.php">・ゲームについて</a>
+</p>
+</font>
+
+<font size="7">
+<p style="text-align: left">
+<a href="page2.php">・メンバー紹介</a>
+</p>
+</font>
+
+<font size="7">
+<p style="text-align: left">
+<a href="page2.php">・ログイン</a>
+</p>
+</font>
+
+<p style="text-align: right">
+<input type="button" onclick="history.back()" value="戻る">
+</p>
+
+</body>
+</html>
