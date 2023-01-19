168aac42b7900921c5ac48f20c9b4d18ebaef8da
daaa0217 <gochiusa2002@gmail.com>
2023-01-19 10:57:39 +0900

    タイトル画面

---
+++ title.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>サイト・タイトル画面</title>
+<style>
+.example{
+    font-size: 200%;
+}
+
+.gradient { 
+	background:linear-gradient(#56C500, #fff); 
+	height: 700px;
+	border: 1px solid #333;
+}
+
+</style>
+
+</head>
+<body>
+<div class="gradient">
+<font size="7">
+3pt チーム　卒業制作ホームページ
+</font>
+<hr></hr>
+
+<p style="text-align: right">
+<input type="button" onclick="location.href='./site.php'" value="メニューへ" class="example">
+</p>
+
+<img src="title_take_5.png">
+
+</div>
+</body>
+</html>
