5a7e97da04fa0ad2aa8b104e922a23919812b9d4
3A_21_香西史郁 <r02.gm.2100252@da.iwasaki.ac.jp>
2023-01-19 09:36:09 +0900

    no message

--- desktop4.php
+++ desktop4.php
@@ -2,19 +2,26 @@
 <head>
 <meta charset="UTF-8">
 <title>テスト</title>
+<style>
+.gradient { 
+    background:linear-gradient(#56C500, #fff); 
+    height: 700px;
+    border: 1px solid #333;
+}
+</style>
 </head>
 <body>
+<div class="gradient">
+
 <h1>3pt チーム　卒業制作ホームページ</h1>
 
 <h2>作品制作</h2><br>
 
 <h2>ゲームについて</h2>
 
-<ol></ol>ビリヤードをもとにしたゲームで多少のギミックが存在します
+<ol>ビリヤードをもとにしたゲームで多少のギミックが存在します。."\n"
+ボールの動きの制御などはunityの物理エンジンに頼ることなく自分たちで制作を行いました</ol>
 
-<h2>成果物</h2>
-
-<ol></ol>コメント
 
 <h2>コンセプト</h2>
 
@@ -24,5 +31,6 @@
 
 <ol></ol>1年
 </table>
+</div>
 </body>
 </html>
