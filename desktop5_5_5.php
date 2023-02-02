38cfeb34e16b449f055af293eeb88521321da877
parasu <parasumara@gmail.com>
2023-02-02 09:58:38 +0900

    小泉

---
+++ desktop5_5_5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+<title>テスト</title>
+</head>
+<body>
+<h1>3pt チーム　卒業制作ホームページ</h1><hr></hr>
+
+<h2>ダウンロード</h2>
+<a href="https://drive.google.com/file/d/1DpLU_ch2OWhd9EjWM_NCct_1HgbTE8k-/view"><b>ゲームダウンロードはこちら</b></a>
+<ol></ol>コメント
+
+</table>
+</body>
+</html>
